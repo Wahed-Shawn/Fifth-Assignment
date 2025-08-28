82f8a89fb25df8fc74ff61c255157a94fd1af97e
Wahed-Shawn <wahedchowdhuryshawn@gmail.com>
2025-08-28 22:04:35 +0600

    added heart count functionality

---
+++ script.js
@@ -0,0 +1,18 @@
+// Heart Count Functionality
+let heartCount = 0
+function heartIcon(id) {
+    document.getElementById(id).addEventListener('click', function () {
+        heartCount++
+        document.getElementById('heartCount').innerText = heartCount
+        document.getElementById(id).classList.add('fa-solid','text-red-600')
+    })
+}
+heartIcon('heart-national')
+heartIcon('heart-police')
+heartIcon('heart-fire')
+heartIcon('heart-ambulance')
+heartIcon('heart-child')
+heartIcon('heart-corruption')
+heartIcon('heart-electricity')
+heartIcon('heart-brac')
+heartIcon('heart-rail')
